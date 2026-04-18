12dace4a1b7782edf0bb1be50ab4e3ceb39ecb9e
soccergrl5 <101655846+soccergrl5@users.noreply.github.com>
2026-04-18 18:00:59 +0200

    Select Course Overview
    Select different courses
    No feedback when clicked

--- app/src/main/java/com/soccergrlstudios/wannmensieren/MainActivity.kt
+++ app/src/main/java/com/soccergrlstudios/wannmensieren/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.soccergrlstudios.wannmensieren
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var courseListDisplay: ListView
+    private lateinit var courseNameList: ArrayList<String>
+    private lateinit var selectedCourseNameList: ArrayList<String>
+    private lateinit var itemAdapter: ArrayAdapter<String>
+
+    private lateinit var okButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +27,36 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        selectCourseOverview()
+    }
+
+    fun selectCourseOverview() {
+        courseNameList = ArrayList()
+        courseNameList.add("Fach1")
+        courseNameList.add("Fach2")
+
+        selectedCourseNameList = ArrayList()
+
+        courseListDisplay = findViewById(R.id.courseList)
+
+        itemAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, courseNameList)
+        courseListDisplay.adapter = itemAdapter
+
+        courseListDisplay.setOnItemClickListener{_, _, pos, _ ->
+            selectedCourseNameList.add(courseNameList.get(pos))
+        }
+
+        okButton = findViewById(R.id.ok)
+        okButton.setOnClickListener {
+            var message: String = ""
+
+            for(name in selectedCourseNameList){
+                message += name + ", "
+            }
+
+            Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
         }
     }
 }
